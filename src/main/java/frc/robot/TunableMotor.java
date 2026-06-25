cfcd0e0b71d07484ebba778227ac2547896a3a15
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-25 14:59:17 +0800

    Start Tunable Motor Class

--- src/main/java/frc/robot/TunableMotor.java
+++ src/main/java/frc/robot/TunableMotor.java
@@ -1,5 +1,30 @@
 package frc.robot;
 
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+
 public interface TunableMotor {
 
+    public void createMotor();
+
+    public void setMotorSpeed();
+
+    public void setMotorVoltage();
+
+    public void setMotorCurrent();
+
+    public void stopMotor();
+
+    public AngularVelocity getMotorVelocity();
+
+    public Angle getMotorPosition();
+
+    public AngularAcceleration getAcceleration();
+
+    public Current getCurrent();
+
+    public Voltage getVoltage();
 }
