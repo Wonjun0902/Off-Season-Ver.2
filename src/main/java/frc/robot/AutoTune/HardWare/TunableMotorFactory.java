09ab5c6d2f852a26a51bf3db57192a621a64c3eb
Wonjun0902 <28wonjuny@students.tas.tw>
2026-07-01 21:43:44 +0800

    Finished selection and creation class

--- src/main/java/frc/robot/AutoTune/HardWare/TunableMotorFactory.java
+++ src/main/java/frc/robot/AutoTune/HardWare/TunableMotorFactory.java
@@ -1,5 +1,22 @@
 package frc.robot.AutoTune.HardWare;
 
+import frc.lib.LazyFXS;
+import frc.lib.LazyTalon;
+
 public class TunableMotorFactory {
 
+    //Creates a Tunable TalonFXS
+    public static TunableMotor createFXSMotor(LazyFXS FXSMotor){
+        return new TunableFXS(FXSMotor);
+    }
+
+    //Creates a Tunable TalonFX
+    public static TunableMotor createTalonMotor(LazyTalon talonMotor){
+        return new TunableTalon(talonMotor);
+    }
+
+    //Creates a Tunable Sim Motor
+    public static TunableMotor createSimMotor(){
+        return new TunableMotorSim();
+    }
 }
