67860df0ed1d40f59d32e16d82917307f68421de
Wonjun0902 <28wonjuny@students.tas.tw>
2026-07-04 09:21:54 +0800

    Create kV Tuning Command class + migrate code

--- src/main/java/frc/robot/AutoTune/Commands/kVTuningCommand.java
+++ src/main/java/frc/robot/AutoTune/Commands/kVTuningCommand.java
@@ -1,5 +1,50 @@
 package frc.robot.AutoTune.Commands;
 
-public class kVTuningCommand {
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.AutoTune.MotorExecute;
+
+public class kVTuningCommand extends SubsystemBase{
+
+    private MotorExecute motorExecute;
+
+    /**
+     * kV Tuning Command 
+     * I set a target speed and apply voltage that increments until the voltage makes the motor turn until 80% of the target speed
+     * When the motor speed reaches 80% of the target speed the motor will stop applying the voltage 
+     * Later in the analyzer class, I will get the voltage that enables the motor to reach 80% speed!
+     * @param setVoltage
+     * @param targetSpeed
+     */
+    private double currentVoltage;
+    public Command kVTuningCommand(AngularVelocity targetSpeed, double voltsPerLoop){
+        //Initialize the volts for 0.0 at the start 
+        currentVoltage = 0.0;
+        return run(
+          () -> {
+            //1. Increment Voltage
+            currentVoltage += voltsPerLoop;
+
+            //2. Apply to Motor 
+            motorExecute.setMotorVoltage(currentVoltage);
+          }
+        )
+        //Reset State before starting 
+        .beforeStarting(() -> {currentVoltage = 0.0;})
+        //Does this until the currentSpeed is greater or equal than the threshold, which is 80 percent of the targetSpeed
+        //Basically making it stop when they are equal
+        .until(() -> {
+            AngularVelocity currentSpeed = motorExecute.getMotorSpeed();
+            AngularVelocity threshold  = targetSpeed.times(0.87);
+
+            return currentSpeed.gte(threshold);
+        })
+        .finallyDo(() -> {
+            motorExecute.stopMotor();
+        });
+    }
 
 }
