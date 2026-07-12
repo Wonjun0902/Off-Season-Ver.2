c4d7cded200556da9703014f1ba97ed9d6263a76
Wonjun0902 <28wonjuny@students.tas.tw>
2026-07-12 13:15:35 +0800

    Start kP Tuning Command

--- src/main/java/frc/robot/AutoTune/Commands/PositionBasedMech/MotionMagic/MMkPTuningCommand.java
+++ src/main/java/frc/robot/AutoTune/Commands/PositionBasedMech/MotionMagic/MMkPTuningCommand.java
@@ -1,5 +1,20 @@
 package frc.robot.AutoTune.Commands.PositionBasedMech.MotionMagic;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.AutoTune.MotorExecute;
+
+import edu.wpi.first.math.filter.LinearFilter;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Commands; 
+
+//Class for caculating the kP Value for MM
+
+//It is very important that you put the right direction of the motor -> positive or negative direction
+//If you don't consider that, the motor will likely stop due to the safety check of the setMotorVoltagePOS method
 public class MMkPTuningCommand {
+    
+    
 
 }
